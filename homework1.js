e8282a5df24d3fde317a7b2a83c17be0e6d232f4
Olga Popova <olga.popova.alex@gmail.com>
2023-06-06 15:37:55 +0300

    Add homework1

---
+++ homework1.js
@@ -0,0 +1,49 @@
+//task 2
+//console.log(Popova);
+//task3
+let first = 'hey';
+let second = 'you';
+ console.log(first + second);
+ first = second;
+ alert(first + second);
+
+ //task4
+ const user = {
+   name: "olga",
+     age: 11,
+   online: true,
+   street: undefined,
+   phone: null
+ }
+
+ //task5
+ let isAudult = confirm("are you 18 years of age or over");
+
+console.log(isAudult);
+
+//task6
+
+
+  let myname= "olga";
+  let surname= "popova";
+  let group= tta22;
+   let bod= 1989;
+   
+let isMarried = true;
+console.log(bod, isMarried, myname, surname);
+let pets = undefined;
+let kids = null;
+
+//task7
+let login = promt("what is you login");
+let email = promt("what is you email");
+let password = promt("what is you password");
+console.log('Dear'+login +", your email is "  + email + " your password is" + password); 
+
+//task 8
+let minute = (1*60);
+let hour = minute*60;
+let month = hour*24*30;
+onsole.log(minute, hour, month);
+
+
